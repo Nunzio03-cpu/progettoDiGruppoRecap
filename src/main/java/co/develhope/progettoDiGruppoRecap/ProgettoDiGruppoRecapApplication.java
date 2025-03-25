61302b24267652999cde1f3e4317d7c6a784a11e
Nunzio <nunziodebartolomeo03@gmail.com>
2025-03-25 12:14:12 +0100

    init

---
+++ src/main/java/co/develhope/progettoDiGruppoRecap/ProgettoDiGruppoRecapApplication.java
@@ -0,0 +1,14 @@
+package co.develhope.progettoDiGruppoRecap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoDiGruppoRecapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoDiGruppoRecapApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/co/develhope/progettoDiGruppoRecap/ProgettoDiGruppoRecapApplicationTests.java
@@ -0,0 +1,13 @@
+package co.develhope.progettoDiGruppoRecap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoDiGruppoRecapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
