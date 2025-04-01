31fec8bc0557fccd43d5fb09b9a56036aea3b0dd
Nunzio <nunziodebartolomeo03@gmail.com>
2025-04-01 15:44:38 +0200

    commit refactoring test

--- src/test/java/co/develhope/progettoDiGruppoRecap/DepartmentTest.java
+++ src/test/java/co/develhope/progettoDiGruppoRecap/DepartmentTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
@@ -26,6 +27,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import java.util.Optional;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 public class DepartmentTest {
     @Autowired
